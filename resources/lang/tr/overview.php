f3e7e9e697bf9ea8d97a6a61c48ea56593a9cba9
kzorluoglu <koray@d8devs.com>
2020-09-26 18:51:34 +0200

    modul initial

---
+++ resources/lang/tr/overview.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'page_title' => 'Genel Bakış',
+    'page_description' => 'Genel Istatistikler',
+    'invoice_number' => 'Fatura No',
+    'date' => 'Tarih',
+    'firstname' => 'Adı',
+    'lastname' => 'Soyadı',
+    'company_name' => 'Firma Adı',
+    'sum' => 'Ara Toplam',
+    'tax' => 'Vergi',
+    'total_sum' => 'Toplam',
+];
---
+++ resources/views/overview/prepare.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('css')
+@parent()
+<style>
+.paid{
+    color: #1e6641;
+    background-color: #d8f3e5;
+    border-color: #c8eedb;
+}
+</style>
+@endsection
+
+@section('content')
+    @include('invoice.invoice')
+@endsection
