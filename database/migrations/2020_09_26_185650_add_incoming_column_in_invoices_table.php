eca61d52c43a8a4979a9eec24ea17c308927d18d
kzorluoglu <koray@d8devs.com>
2022-08-12 21:44:21 +0200

    ref: #update: laravel up to 9 upgraded, user:company:product:customer seeders added, model fixes

--- database/migrations/2020_09_26_185650_add_incoming_column_in_invoices_table.php
+++ database/migrations/2020_09_26_185650_add_incoming_column_in_invoices_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddIncomingColumnInInvoivesTable extends Migration
+class AddIncomingColumnInInvoicesTable extends Migration
 {
     /**
      * Run the migrations.
